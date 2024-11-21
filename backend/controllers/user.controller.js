f72c5784af0adfc626af4ee9d7cb4c12e1383c9f
Saquib khan <saquibkhan122335@gmail.com>
2024-11-21 23:23:25 +0530

    Initialize admin folder structure and add 'Add Product' page

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -97,7 +97,7 @@ const adminLogin = async (req, res, next) => {
         if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
             const token = jwt.sign(email+password,process.env.JWT_SECRET)
             res.json({success:true,token})
-            console.log(token)
+            
         }else{
             res.json({success:false,message:'Invalid credentials'})
         }
